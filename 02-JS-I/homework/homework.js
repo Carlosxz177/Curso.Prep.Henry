8f44cc5ec527f693dcf2f7faba9394819b37b089
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-13 18:36:16 -0300

    Ejercicio 3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 128;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === false;
+const nuevaResta = 10 - 5 === 5;
 
 
 // Resuelve el siguiente problema matemático:
