be9c706aa71414348b66e1b8761fcbf92db247c2
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-13 18:50:57 -0300

    seisysiete

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -17,7 +17,7 @@ const nuevaResta = 10 - null === 10;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 4,2;
+const nuevoModulo = 21 % 5 === null;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -29,6 +29,7 @@ const nuevoModulo = 21 % 5 === 4,2;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return(str)
   
 }
 
