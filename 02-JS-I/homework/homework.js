d39d343de29aaf5d733d0bce886c70a6edb3cb8b
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-13 21:57:47 -0300

    nuevo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,7 +74,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(str1 === str2) {
+  if(str1 == str2) {
 
   } else(str1 < str2);{
 
@@ -87,9 +87,9 @@ function menosQueNoventa(num) {
   // Tu código:
   num = 25
   if (num < 90){
-   num = 25
+   
   } else(num > 90); {
-   num = 25
+   
 
   }
   
