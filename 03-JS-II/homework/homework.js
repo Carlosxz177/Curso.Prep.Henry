55adbabcaf11f2abb9bd4114cc3e6b8aedb7252a
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-15 13:47:32 -0300

    etapatwo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -80,12 +80,20 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5){
+    return true
+  }
+  return false 
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero > 20 && numero < 50){
+  return true
+}
+return false
 }
 
 function esEntero(numero) {
@@ -96,6 +104,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return Math.floor(numero);
 }
 
 function fizzBuzz(numero) {
