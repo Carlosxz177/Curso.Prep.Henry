4b521c34cda648e54ace5910299b7578dd8b1efe
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-15 14:39:16 -0300

    etapatwo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -166,10 +166,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-if (valor === 5){
-  return "Soy verdadero"
+if (valor === true){
+  return "Soy verdadero";
  }else {
- return "Soy falso"
+ return "Soy falso";
   }
 }
 
