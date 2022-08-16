a1cc52e0cefdc8b01cc9ce0eb8166d40c8e139de
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-16 17:15:32 -0300

    ejercicioFour

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -38,7 +38,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  return agregarItemAlFinalDelArray.push[elem];
+  return agregarItemAlFinalDelArray.push[elemento];
 }
 
 
@@ -47,7 +47,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  return agregarItemAlComienzoDelArray.unshift;
+  return agregarItemAlComienzoDelArray.unshift[elemento];
 }
 
 
