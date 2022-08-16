f0eb672c64b1373cc7fbf56091a070574469c715
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-16 18:14:32 -0300

    ejercicioFour

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -92,12 +92,8 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var promedio = 0
-  for( var i = 0 ; i < resultadosTest.length ; i++){
-
-  promedio = promedio % resultadosTest[i];
-  }
-  return promedio;
+ 
+  return nuevoResultado(resultadosTest) / resultadosTest.length;
 }
 
 
