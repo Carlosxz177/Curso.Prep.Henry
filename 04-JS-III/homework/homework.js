0be311fc9a48d455df2a21b8ff96821d1256b96f
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-16 17:17:56 -0300

    ejercicioFour

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -47,7 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  return agregarItemAlComienzoDelArray.unshift[elemento];
+  array.unshift(elemento);
+  return array;
 }
 
 
