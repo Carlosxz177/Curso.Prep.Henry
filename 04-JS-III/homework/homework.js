366ff69f9eb935016d7dad11a2f9aeeba9f7e482
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-16 19:16:27 -0300

    ejercicioFour

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -153,8 +153,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  if(n === 9){
+    return true;
+  }
+  return false;
 }
+
 
 
 function todosIguales(arreglo) {
