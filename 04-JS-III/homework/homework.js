ba83149cbf47370a3ba3ce6ec98ab6980b888d26
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-16 17:46:46 -0300

    ejercicioFour

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -67,10 +67,14 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  array[array.length] = elemento;
-  return true;
+  for ( var i = 0 ; i < array.length ; i++ ){
+    if (array[i] === elemento){
+      return true;
+    }
+    return false;
+  }
 }
-  return false;
+ 
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
