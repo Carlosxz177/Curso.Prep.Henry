52ab371b7eb7818c538d66d17144830236f696a5
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-17 10:14:31 -0300

    ejercicioFive

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -58,7 +58,10 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var objeto = {
+    usuario : kapoloco
+  }
+   return objeto;
 }
 
 function tieneEmail(usuario) {
