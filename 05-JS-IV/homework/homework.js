59904a911f3243a6571ba4404b24eed1e0603e79
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-17 10:27:52 -0300

    ejercicioFive

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -94,6 +94,7 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  return usuario["password"] === password; 
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
