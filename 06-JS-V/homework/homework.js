f00c783429fdfd581e0b79776b2b9b49c4e4e916
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-18 15:51:46 -0300

    ejercicioSix

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -88,6 +88,9 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  persona.prototype.datos = function(){
+    return this.nombre + "," + this.edad + "años"; 
+  }
 }
   
 
