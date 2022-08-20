23960dce33136d626685b7888a65d0ebfd05f788
Carlox <adrian.carlos.ledesma@gmail.com>
2022-08-20 02:11:52 -0300

    TestSix

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -50,7 +50,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-
+   var nuevoArray = array.map(function(longitud){
+        return cb(longitud);
+   });
+   return nuevoArray;
 }
 
 function filter(array) {
